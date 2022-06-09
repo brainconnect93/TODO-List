24b448281f158eda6139677de0434a0105db6224
Afolabi Akorede <braincommect0@gmail.com>
2022-06-10 00:56:57 +0100

    Check Linters

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import './styles.css';
-import { toDoList } from './modules/addRemove';
+import ToDoList from './modules/addRemove.js';
 
-const taskstore = new toDoList();
-
+const taskstore = new ToDoList();
 
 const textInput = document.querySelector('#add-items');
 
 textInput.addEventListener('keypress', (e) => {
-  if(e.key === 'Enter' && textInput.value){
-      e.preventDefault();
-      taskstore.addTask(textInput.value);
-      textInput.value = null;
-  };
+  if (e.key === 'Enter' && textInput.value) {
+    e.preventDefault();
+    taskstore.addTask(textInput.value);
+    textInput.value = null;
+  }
 });
 
 window.addEventListener('load', taskstore.displayTodos);
