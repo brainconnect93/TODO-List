38933a8a8b75e9e6cee8633af007238fe2db14b2
brainconnect93 <brainconnect0@gmail.com>
2022-09-20 23:34:16 +0100

    Fix the eslint disable error

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,34 @@
-/* eslint-disable */
-
-const path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: "./src/index.js",
-    module: {
-        rules: [
-            {
-                test: /\.svg$/,
-                use: 'svg-inline-loader',
-            },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(js)$/,
-                use: "babel-loader",
-            },
-        ],
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: 'svg-inline-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(js)$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  plugins: [new HtmlWebpackPlugin(
+    {
+      template: path.resolve(__dirname, './src/index.html'),
     },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
-        clean: true,
-    },
-    plugins: [new HtmlWebpackPlugin(
-        {
-            template: path.resolve(__dirname, './src/index.html'),
-        },
-      
-    )],
-    mode: process.env.NODE_ENV === "production" ? "production" : "development",
+
+  )],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 };
